cb8dadf7c1ddbe584d4c7e57a180b70ae92209db
Marc Baque <marc.baque@nubos.cloud>
2026-07-05 16:15:23 +0200

    feat: add environment variable support for API URL and update fetch calls in screener queries

--- apps/website/src/data/screener.queries.ts
+++ apps/website/src/data/screener.queries.ts
@@ -2,6 +2,8 @@ import { defineQuery, useQuery } from '@pinia/colada';
 import type { ScreenerRow } from '../../../api/main';
 import { ref } from 'vue';
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export const QUERY_KEY_SCREENER = {
   root: ['screener'] as const,
   fivePillars: () => [...QUERY_KEY_SCREENER.root, 'five-pillars'] as const,
@@ -12,7 +14,7 @@ export const QUERY_KEY_SCREENER = {
 export const fivePillarsQuery = defineQuery({
   key: QUERY_KEY_SCREENER.fivePillars(),
   query: async () => {
-    const res = await fetch('http://localhost:8000/screener/pillars-momentum');
+    const res = await fetch(`${API_URL}/screener/pillars-momentum`);
 
     return res.json() as unknown as ScreenerRow[];
   },
@@ -22,7 +24,7 @@ export const fivePillarsQuery = defineQuery({
 export const highestGainersQuery = defineQuery({
   key: QUERY_KEY_SCREENER.highestGainers(),
   query: async () => {
-    const res = await fetch('http://localhost:8000/screener/highest-gainers');
+    const res = await fetch(`${API_URL}/screener/highest-gainers`);
 
     return res.json() as unknown as ScreenerRow[];
   },
@@ -35,7 +37,7 @@ export const newsQuery = defineQuery(() => {
   const { data, isLoading } = useQuery({
     key: () => QUERY_KEY_SCREENER.news(ticker.value),
     query: async () => {
-      const res = await fetch(`http://localhost:8000/news/${ticker.value}`);
+      const res = await fetch(`${API_URL}/news/${ticker.value}`);
 
       return res.json() as unknown as ScreenerRow[];
     },
